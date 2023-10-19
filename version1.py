f4619dffc2d6c8470c7db78547050ba4fcf38160
Pooja Nagapurkar <147425607+poojan456@users.noreply.github.com>
2023-10-19 11:47:37 +0530

    Update version1.py

--- version1.py
+++ version1.py
@@ -11,9 +11,17 @@ def generateOTP(length):
         otp += digits[math.floor(random.random()*10)]
     return otp
 
-try:
-   connection.sendmail(from_addr=my_mail, to_addrs="pooja1713953@gmail.com",msg=mail_content)
-except Exception as e:
-   print("Something went wrong while sending the email", e)
+def ValidateMobileNo(MobileNo):
+    if len(MobileNo) != 10:
+        print("Please enter valid Mobile number!!")
+        MobileNo = input("Enter the Mobile number:")
+        ValidateMobileNo(MobileNo)
+    return MobileNo
 
-connection.close()
+
+def ValidateEmailID(Email):
+    if "@gmail.com" not in Email:
+        print("Please Enter Valid Email address!!")
+        Email = input("Enter the Email:")
+        ValidateEmailID(Email)
+    return Email
